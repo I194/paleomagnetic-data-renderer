73b3bc0406e1587db3edbe49b061a2d9cfe4ffb4
I194 <i1948374@yandex.ru>
2021-12-29 11:36:00 +0300

    example of dynamically rendered dots on ZijdGraph AND this dots have events on them

--- src/components/ZijdGraph/ZijdGraph.tsx
+++ src/components/ZijdGraph/ZijdGraph.tsx
@@ -1,60 +1,55 @@
 import React, { FC } from "react";
 import styles from './ZijdGraph.module.scss';
 
-const Circle: FC = () => {
+interface ICircle {
+  x: number;
+  y: number;
+  r?: number;
+  id: string;
+}
+
+const Circle: FC<ICircle> = ({x, y, r, id}) => {
 
   const handleClick = () => {
     console.log('a');
+    alert('Нежнее!')
   }
 
-  const handleOver = () => {
-    document.getElementById('dot1')?.style.setProperty('fill', 'red');
+  const handleOver = (id: string) => {
+    document.getElementById(id)?.style.setProperty('fill', 'orange');
   }
 
-  const handleOut = () => {
-    document.getElementById('dot1')?.style.setProperty('fill', 'black');
+  const handleOut = (id: string) => {
+    document.getElementById(id)?.style.setProperty('fill', 'black');
   }
 
   return (
     <circle 
-      cx="25" 
-      cy="25" 
-      r="10"
-      id="dot1"
+      cx={x} 
+      cy={y} 
+      r={r ? r : 4}
+      id={id}
       style={{
         fill: 'black', 
-        stroke: 'black'
+        stroke: 'black',
+        cursor: 'pointer'
       }} 
       className={styles.dot}
       onClick={handleClick}
-      onMouseOver={handleOver}
-      onMouseOut={handleOut}
+      onMouseOver={() => handleOver(id)}
+      onMouseOut={() => handleOut(id)}
     />
   )
 }
 
 const ZijdGraph: FC = () => {
 
-
+  const XYdata = [[20, 20], [25, 70], [50, 40], [39, 72], [110, 119], [118, 129], [134, 141], [150, 150]]
 
   return (
     <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300px" height="300px" id='mySVG'>
       <line x1="150" y1="0" x2="150" y2="300" stroke="black" strokeWidth="2" />
       <line x1="0" y1="150" x2="300" y2="150" stroke="black" strokeWidth="2" />
-      
-      <defs>
-        <marker id="mC" markerWidth="10" markerHeight="10" refX="5" refY="5">
-          <circle 
-            cx="5" 
-            cy="5" 
-            r="2" 
-            style={{
-              fill: 'black', 
-              stroke: 'black'
-            }} 
-          />
-        </marker>
-      </defs>
       {/* 
           Создавать маркеры черезе path нельзя, ибо тогда теряется почти весь их функционал
           Добавить слушатель можно только к конкретному элементу по типу <circle />
@@ -62,15 +57,12 @@ const ZijdGraph: FC = () => {
           Однако hover всё равно работать не будет и потому лучше использовать onMouseOver
           Как раз при этом достигается условие zero-css (я его только что сам придумал)
       */}
-      <Circle />
       <path 
         d="M20,20 L25,70 L50,40 L39,72, L110,119, L118,129, L134,141, L150,150" 
-        markerStart="url(#mC)" 
-        markerMid="url(#mC)" 
-        markerEnd="url(#mC)" 
         fill="none" 
         stroke=" black" 
       />
+      {XYdata.map((xy, iter) => <Circle x={xy[0]} y={xy[1]} id={`dot${iter}`}/>)}
     </svg>
   )
 }
