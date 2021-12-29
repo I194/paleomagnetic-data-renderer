9c8f022b264e1ab448fcfbccd7c275cf861fa179
I194 <i1948374@yandex.ru>
2021-12-29 11:22:38 +0300

    first try of adding events to markers

--- src/components/ZijdGraph/ZijdGraph.tsx
+++ src/components/ZijdGraph/ZijdGraph.tsx
@@ -1,6 +1,41 @@
 import React, { FC } from "react";
+import styles from './ZijdGraph.module.scss';
+
+const Circle: FC = () => {
+
+  const handleClick = () => {
+    console.log('a');
+  }
+
+  const handleOver = () => {
+    document.getElementById('dot1')?.style.setProperty('fill', 'red');
+  }
+
+  const handleOut = () => {
+    document.getElementById('dot1')?.style.setProperty('fill', 'black');
+  }
+
+  return (
+    <circle 
+      cx="25" 
+      cy="25" 
+      r="10"
+      id="dot1"
+      style={{
+        fill: 'black', 
+        stroke: 'black'
+      }} 
+      className={styles.dot}
+      onClick={handleClick}
+      onMouseOver={handleOver}
+      onMouseOut={handleOut}
+    />
+  )
+}
 
 const ZijdGraph: FC = () => {
+
+
 
   return (
     <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300px" height="300px" id='mySVG'>
@@ -9,9 +44,25 @@ const ZijdGraph: FC = () => {
       
       <defs>
         <marker id="mC" markerWidth="10" markerHeight="10" refX="5" refY="5">
-          <circle cx="5" cy="5" r="2" style={{fill: 'black', stroke: 'black'}}/>
+          <circle 
+            cx="5" 
+            cy="5" 
+            r="2" 
+            style={{
+              fill: 'black', 
+              stroke: 'black'
+            }} 
+          />
         </marker>
       </defs>
+      {/* 
+          Создавать маркеры черезе path нельзя, ибо тогда теряется почти весь их функционал
+          Добавить слушатель можно только к конкретному элементу по типу <circle />
+          Потому лучше отрисовывать отдельно каждый <circle /> через map массива координат
+          Однако hover всё равно работать не будет и потому лучше использовать onMouseOver
+          Как раз при этом достигается условие zero-css (я его только что сам придумал)
+      */}
+      <Circle />
       <path 
         d="M20,20 L25,70 L50,40 L39,72, L110,119, L118,129, L134,141, L150,150" 
         markerStart="url(#mC)" 
